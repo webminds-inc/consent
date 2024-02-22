cf08e370bb588f5c498e6e9629b35eb6b7c995a0
Thuy Nguyen <ducthuy@gmail.com>
2024-02-22 21:49:04 +0700

    Update

--- _scripts/build.js
+++ _scripts/build.js
@@ -7,24 +7,7 @@ const files = [
     "./src/js/consent.js",
 ];
 
-function convertToBase(number) {
-    if (isNaN(number) || number < 0 || !Number.isInteger(number)) {
-        return "Invalid input";
-    }
-
-    const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let result = "";
-
-    do {
-        result = characters[number % characters.length] + result;
-        number = Math.floor(number / characters.length);
-    } while (number > 0);
-
-    return result;
-}
-
 async function compress() {
-    //' + convertToBase((new Date()).getTime()) + '
     let filename = './dist/consent.min.js';
     let originalCode = "";
     files.forEach(js => {
